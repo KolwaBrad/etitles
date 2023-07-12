886dc6d5f7ff59cf4b4e2f2204a67d2eb3a0398a
Sydney-Nyanchoga <sydney.nyanchoga@strathmore.edu>
2023-07-13 01:06:34 +0300

    blockchain files

---
+++ resources/views/blockcommands.blade.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+</head>
+<body>
+    <h1>my commands</h1>
+    <a href="addblocks">add blocks</a>
+    <a href="addmoreblocks">add more blocks</a>
+    <a href="updateblocks">update blocks</a>
+    <a href="index">view blockchain</a>
+</body>
+</html>
